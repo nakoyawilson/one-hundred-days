0c23324a2e7d9e10ae4e98ef3b0ace2a4b78c597
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-21 18:26:00 -0400

    Change title() method to string.capwords() method
    Changed title() method to string.capwords() method for cases where
    user inputs words with apostrophe, etc.

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,3 +1,4 @@
+import string
 # 1. Create a greeting for your program.
 print("Hello! Welcome to the Band Name Generator!")
 # 2. Ask the user for the city that they grew up in.
@@ -5,4 +6,4 @@ city = input("What is the name of the city that you grew up in?\n")
 # 3. Ask the user for the name of a pet.
 pet = input("What was the name of your first pet?\n")
 # 4. Combine the name of their city and pet and show them their band name.
-print("Your band name could be " + city.title() + " " + pet.title() + ".")
+print("Your band name could be " + string.capwords(city) + " " + string.capwords(pet) + ".")
