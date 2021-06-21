49dd1b15c5f276feb52d4cf35fb479e9cab54f51
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-21 18:15:50 -0400

    Add a space between the city and pet name

--- band_name_generator.py
+++ band_name_generator.py
@@ -5,4 +5,4 @@ city = input("What is the name of the city that you grew up in?\n")
 # 3. Ask the user for the name of a pet.
 pet = input("What was the name of your first pet?\n")
 # 4. Combine the name of their city and pet and show them their band name.
-print("Your band name is " + city + pet + ".")
+print("Your band name is " + city + " " + pet + ".")
