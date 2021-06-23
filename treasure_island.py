650f32af60baa501c3e294a3c89472e9aea9bc8c
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-23 16:20:20 -0400

    Add conditional statements

---
+++ treasure_island.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("Question that prompts player to choose direction.")
+direction = input("Which way do you go (left or right)? ").lower()
+if direction == "left":
+    print("Question that prompts player to choose swim or wait.")
+    transport = input("Do you swim or wait? ").lower()
+    if transport == "wait":
+        print("Question that prompts user to choose color door.")
+        color = input("Which door do you go through (red, yellow or blue)? ").lower()
+        if color == "yellow":
+            print("You find the treasure. You win!")
+        elif color == "red":
+            print("Burned by fire. Game Over.")
+        elif color == "blue":
+            print("Eaten by beasts. Game Over.")
+        else:
+            print("You didn't choose a door. Game Over.")
+else:
+    print("Fall into a hole. Game Over.")
