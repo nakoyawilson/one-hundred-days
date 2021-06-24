591b0b6946847b45a0b0c40666919d76e8c0daea
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-24 18:56:19 -0400

    Add code for Rock Paper Scissors game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,23 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+choice_list = [rock, paper, scissors]
+print("Welcome to Rock Paper Scissors")
+print("The rules to play are pretty simple.")
+print("The player and the computer each choose a number that will represent the elements of the game: rock, paper and scissors.")
+print("The outcome of the game is determined by 3 simple rules:")
+print("\t- Rock wins against scissors.")
+print("\t- Scissors win against paper.")
+print("\t- Paper wins against rock.")
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(choice_list[player_choice])
+computer_choice = random.randint(0,2)
+print("Computer chose:")
+print(choice_list[computer_choice])
+if player_choice == computer_choice:
+    print("It's a tie.")
+if (player_choice == 0 and computer_choice == 2) or (player_choice == 2 and computer_choice == 1) or (player_choice == 1 and computer_choice == 0):
+    print("You win!")
+else:
+    print("You lose.")
