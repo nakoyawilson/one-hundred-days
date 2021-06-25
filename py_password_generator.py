cefa9f49152fbe4cdbf619057faf728734e1b860
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-25 08:00:19 -0400

    Add code to create password where character order not randomised

--- py_password_generator.py
+++ py_password_generator.py
@@ -15,3 +15,14 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password = ""
+for letter in range(1, nr_letters + 1):
+    random_letter = random.choice(letters)
+    password += random_letter
+for symbol in range(1, nr_symbols + 1):
+    random_symbol = random.choice(symbols)
+    password += random_symbol
+for number in range(1, nr_numbers + 1):
+    random_number = random.choice(numbers)
+    password += random_number
+print(f"Your password is: {password}")
