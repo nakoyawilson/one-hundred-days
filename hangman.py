e94d1e8a583601dcdaad3a32e1aeb4d2faae17fa
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-27 10:21:08 -0400

    Add code to randomly choose word from word_list

--- hangman.py
+++ hangman.py
@@ -1,8 +1,11 @@
+import random
+
 #Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
