00f9ac0b2ff0c1124adae145033266c30f0295cb
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-27 10:50:00 -0400

    Add code to display a blank line for each letter in chosen_word

--- hangman.py
+++ hangman.py
@@ -13,9 +13,13 @@ print(f'Pssst, the solution is {chosen_word}.')
 #TODO-1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for letter in range(word_length):
+    display.append("_")
+print(display)
 
 # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
@@ -24,8 +28,10 @@ guess = input("Guess a letter: ").lower()
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 if guess in chosen_word:
     # Reveal letter
+    pass
 else:
     # Lose a life
+    pass
 
 # TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 # Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
