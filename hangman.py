eb8df9a52d6e10e79b5be0418367bf40ca326ba2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-27 11:44:40 -0400

    Add instructions for #Step 3

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 import random
 
-#Step 2
+#Step 3
 
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -10,21 +10,19 @@ chosen_word = random.choice(word_list)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+# Create an empty List called display.
 display = []
 word_length = len(chosen_word)
 for letter in range(word_length):
     display.append("_")
 
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
 # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
 if guess in chosen_word:
     # Reveal letter
     for index, letter in enumerate(chosen_word):
@@ -34,6 +32,5 @@ else:
     # Lose a life
     pass
 
-# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+# Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 print(display)
