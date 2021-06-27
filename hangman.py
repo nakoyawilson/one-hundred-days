c13ecfb8ea32fa8a4b260e2bf5db8afb4122113f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-27 11:48:37 -0400

    Add while loop to let user guess again

--- hangman.py
+++ hangman.py
@@ -17,20 +17,23 @@ for letter in range(word_length):
     display.append("_")
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while "_" in display:
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+    # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter: ").lower()
 
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-if guess in chosen_word:
-    # Reveal letter
-    for index, letter in enumerate(chosen_word):
-        if letter == guess:
-            display[index] = guess
-else:
-    # Lose a life
-    pass
+    if guess in chosen_word:
+        # Reveal letter
+        for index, letter in enumerate(chosen_word):
+            if letter == guess:
+                display[index] = guess
+    else:
+        # Lose a life
+        pass
 
-# Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-print(display)
+    # Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+    print(display)
+
+print("You win!")
