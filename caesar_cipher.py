cfdb451bb61f1c3eb15ed9fbefaef2569c17207d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 09:38:20 -0400

    Change variable name from key to shift

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,27 +3,27 @@ alphabet = ["a", "b" ,"c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 message = input("Type message here: ")
 method = input("Would you like to encode or decode? ")
-key = int(input("What is the key? "))
+shift = int(input("Type the shift number:\n"))
 
 new_message = ""
 if method == "encode":
     for char in message:
-        if alphabet.index(char) + key < 26:
-            new_index = alphabet.index(char) + key
+        if alphabet.index(char) + shift < 26:
+            new_index = alphabet.index(char) + shift
             new_char = alphabet[new_index]
             new_message += new_char
         else:
-            new_index = alphabet.index(char) + key - 26
+            new_index = alphabet.index(char) + shift - 26
             new_char = alphabet[new_index]
             new_message += new_char
 else:
     for char in message:
-        if alphabet.index(char) - key >= 0:
-            new_index = alphabet.index(char) - key
+        if alphabet.index(char) - shift >= 0:
+            new_index = alphabet.index(char) - shift
             new_char = alphabet[new_index]
             new_message += new_char
         else:
-            new_index = alphabet.index(char) + 26 -key
+            new_index = alphabet.index(char) + 26 - shift
             new_char = alphabet[new_index]
             new_message += new_char
 print(new_message)
