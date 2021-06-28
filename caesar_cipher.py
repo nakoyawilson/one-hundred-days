b6b759bc81cf4668901855febcbc104463c5cd84
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 10:28:07 -0400

    Recombine encrypt() and decrypt() into one function caesar()

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,33 +5,28 @@ text = input("Type your message:\n").lower()
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for char in plain_text:
-        if alphabet.index(char) + shift_amount < 26:
-            new_index = alphabet.index(char) + shift_amount
-            new_char = alphabet[new_index]
-            cipher_text += new_char
-        else:
-            new_index = alphabet.index(char) + shift_amount - 26
-            new_char = alphabet[new_index]
-            cipher_text += new_char
-    print(cipher_text)
+def caesar(start_text, cipher_direction, shift_amount):
+    new_text = ""
+    if cipher_direction == "encode":
+        for char in start_text:
+            if alphabet.index(char) + shift_amount < 26:
+                new_index = alphabet.index(char) + shift_amount
+                new_char = alphabet[new_index]
+                new_text += new_char
+            else:
+                new_index = alphabet.index(char) + shift_amount - 26
+                new_char = alphabet[new_index]
+                new_text += new_char
+    else:
+        for char in start_text:
+            if alphabet.index(char) - shift_amount >= 0:
+                new_index = alphabet.index(char) - shift_amount
+                new_char = alphabet[new_index]
+                new_text += new_char
+            else:
+                new_index = alphabet.index(char) + 26 - shift_amount
+                new_char = alphabet[new_index]
+                new_text += new_char
+    print(new_text)
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for char in cipher_text:
-        if alphabet.index(char) - shift >= 0:
-            new_index = alphabet.index(char) - shift_amount
-            new_char = alphabet[new_index]
-            plain_text += new_char
-        else:
-            new_index = alphabet.index(char) + 26 - shift_amount
-            new_char = alphabet[new_index]
-            plain_text += new_char
-    print(plain_text)
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-else:
-    decrypt(cipher_text=text, shift_amount=shift)
+caesar(start_text=text, cipher_direction=direction,shift_amount=shift)
