d84bda0add5c51192071ebe4ce10f4617dcba100
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 09:53:11 -0400

    Change variable name from new_message to new_text

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,27 +6,27 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 shift = int(input("Type the shift number:\n"))
 
 def encrypt(plain_text, shift_amount):
-    new_message = ""
+    new_text = ""
     if direction == "encode":
         for char in plain_text:
             if alphabet.index(char) + shift_amount < 26:
                 new_index = alphabet.index(char) + shift_amount
                 new_char = alphabet[new_index]
-                new_message += new_char
+                new_text += new_char
             else:
                 new_index = alphabet.index(char) + shift_amount - 26
                 new_char = alphabet[new_index]
-                new_message += new_char
+                new_text += new_char
     else:
         for char in plain_text:
             if alphabet.index(char) - shift >= 0:
                 new_index = alphabet.index(char) - shift_amount
                 new_char = alphabet[new_index]
-                new_message += new_char
+                new_text += new_char
             else:
                 new_index = alphabet.index(char) + 26 - shift_amount
                 new_char = alphabet[new_index]
-                new_message += new_char
-    print(new_message)
+                new_text += new_char
+    print(new_text)
 
 encrypt(text,shift)
