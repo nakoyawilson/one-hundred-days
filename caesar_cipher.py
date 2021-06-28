c0601a0cdfdc913d49a0e66299466acec00c80ac
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 09:46:26 -0400

    Create encrypt() function that takes 'text' and 'shift' as inputs

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,25 +5,28 @@ text = input("Type your message:\n").lower()
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 shift = int(input("Type the shift number:\n"))
 
-new_message = ""
-if direction == "encode":
-    for char in text:
-        if alphabet.index(char) + shift < 26:
-            new_index = alphabet.index(char) + shift
-            new_char = alphabet[new_index]
-            new_message += new_char
-        else:
-            new_index = alphabet.index(char) + shift - 26
-            new_char = alphabet[new_index]
-            new_message += new_char
-else:
-    for char in text:
-        if alphabet.index(char) - shift >= 0:
-            new_index = alphabet.index(char) - shift
-            new_char = alphabet[new_index]
-            new_message += new_char
-        else:
-            new_index = alphabet.index(char) + 26 - shift
-            new_char = alphabet[new_index]
-            new_message += new_char
-print(new_message)
+def encrypt(text, shift):
+    new_message = ""
+    if direction == "encode":
+        for char in text:
+            if alphabet.index(char) + shift < 26:
+                new_index = alphabet.index(char) + shift
+                new_char = alphabet[new_index]
+                new_message += new_char
+            else:
+                new_index = alphabet.index(char) + shift - 26
+                new_char = alphabet[new_index]
+                new_message += new_char
+    else:
+        for char in text:
+            if alphabet.index(char) - shift >= 0:
+                new_index = alphabet.index(char) - shift
+                new_char = alphabet[new_index]
+                new_message += new_char
+            else:
+                new_index = alphabet.index(char) + 26 - shift
+                new_char = alphabet[new_index]
+                new_message += new_char
+    print(new_message)
+
+encrypt(text,shift)
