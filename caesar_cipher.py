421a6f16b76e3cd14618e0accc8b9003c6bb7b11
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 09:50:26 -0400

    Rename text to plain_text and shift to shift_amount

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,26 +5,26 @@ text = input("Type your message:\n").lower()
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
+def encrypt(plain_text, shift_amount):
     new_message = ""
     if direction == "encode":
-        for char in text:
-            if alphabet.index(char) + shift < 26:
-                new_index = alphabet.index(char) + shift
+        for char in plain_text:
+            if alphabet.index(char) + shift_amount < 26:
+                new_index = alphabet.index(char) + shift_amount
                 new_char = alphabet[new_index]
                 new_message += new_char
             else:
-                new_index = alphabet.index(char) + shift - 26
+                new_index = alphabet.index(char) + shift_amount - 26
                 new_char = alphabet[new_index]
                 new_message += new_char
     else:
-        for char in text:
+        for char in plain_text:
             if alphabet.index(char) - shift >= 0:
-                new_index = alphabet.index(char) - shift
+                new_index = alphabet.index(char) - shift_amount
                 new_char = alphabet[new_index]
                 new_message += new_char
             else:
-                new_index = alphabet.index(char) + 26 - shift
+                new_index = alphabet.index(char) + 26 - shift_amount
                 new_char = alphabet[new_index]
                 new_message += new_char
     print(new_message)
