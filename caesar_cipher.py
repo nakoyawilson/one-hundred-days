297d3e500736ec63135521518cfb24de9c566ecc
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 09:36:42 -0400

    Change variable name from letters to alphabet

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,4 +1,4 @@
-letters = ["a", "b" ,"c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+alphabet = ["a", "b" ,"c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 message = input("Type message here: ")
@@ -8,22 +8,22 @@ key = int(input("What is the key? "))
 new_message = ""
 if method == "encode":
     for char in message:
-        if letters.index(char) + key < 26:
-            new_index = letters.index(char) + key
-            new_char = letters[new_index]
+        if alphabet.index(char) + key < 26:
+            new_index = alphabet.index(char) + key
+            new_char = alphabet[new_index]
             new_message += new_char
         else:
-            new_index = letters.index(char) + key - 26
-            new_char = letters[new_index]
+            new_index = alphabet.index(char) + key - 26
+            new_char = alphabet[new_index]
             new_message += new_char
 else:
     for char in message:
-        if letters.index(char) - key >= 0:
-            new_index = letters.index(char) - key
-            new_char = letters[new_index]
+        if alphabet.index(char) - key >= 0:
+            new_index = alphabet.index(char) - key
+            new_char = alphabet[new_index]
             new_message += new_char
         else:
-            new_index = letters.index(char) + 26 -key
-            new_char = letters[new_index]
+            new_index = alphabet.index(char) + 26 -key
+            new_char = alphabet[new_index]
             new_message += new_char
 print(new_message)
