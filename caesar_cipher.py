34e0ac91082fb806ca6b6e1d99a1741d0d3c3027
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-28 10:46:05 -0400

    Import and print logo when program starts

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,9 +1,7 @@
+from caesar_cipher_art import logo
+
 alphabet = ["a", "b" ,"c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-text = input("Type your message:\n").lower()
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-shift = int(input("Type the shift number:\n"))
 
 def caesar(start_text, cipher_direction, shift_amount):
     end_text = ""
@@ -27,6 +25,12 @@ def caesar(start_text, cipher_direction, shift_amount):
                 new_index = alphabet.index(char) + 26 - shift_amount
                 new_char = alphabet[new_index]
                 end_text += new_char
-    print(end_text)
+    print(f"The {cipher_direction}d text is {end_text}")
+
+# Import and print logo from caesar_cipher_Art.py when the program starts
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 caesar(start_text=text, cipher_direction=direction,shift_amount=shift)
