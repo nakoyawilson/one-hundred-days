c878eefd688d77c9ea262c1c1aafcd0bcd12127b
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-29 12:01:56 -0400

    Create dictionary of submitted bids

--- blind_auction.py
+++ blind_auction.py
@@ -1,10 +1,12 @@
 from replit import clear
 
 continue_auction = "yes"
+auction_bids = {}
 while continue_auction == "yes":
   name = (input("What is your name? "))
   bid = float(input("What is your bid? $"))
   continue_auction = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  auction_bids[name] = bid
   clear()
-  
+
 auction_result = f"The winner is {name} with a bid of ${bid:.2f}"
