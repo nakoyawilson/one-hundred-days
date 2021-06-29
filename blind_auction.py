2730e83450af0a21cc669083f73824175fe97dae
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-29 11:53:15 -0400

    Create variables

--- blind_auction.py
+++ blind_auction.py
@@ -1,1 +1,6 @@
 from replit import clear
+
+name = (input("What is your name? "))
+bid = float(input("What is your bid? $"))
+continue_auction = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+auction_result = f"The winner is {name} with a bid of ${bid:.2f}"
