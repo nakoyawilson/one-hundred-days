5b743309034716a49e3e9e7de2619fa43c407bee
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-29 11:57:57 -0400

    Create while loop

--- blind_auction.py
+++ blind_auction.py
@@ -1,6 +1,10 @@
 from replit import clear
 
-name = (input("What is your name? "))
-bid = float(input("What is your bid? $"))
-continue_auction = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+continue_auction = "yes"
+while continue_auction == "yes":
+  name = (input("What is your name? "))
+  bid = float(input("What is your bid? $"))
+  continue_auction = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  clear()
+  
 auction_result = f"The winner is {name} with a bid of ${bid:.2f}"
