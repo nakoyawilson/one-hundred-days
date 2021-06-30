ae14e782c9608976a86727359829e4f4af6c731d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-30 11:59:50 -0400

    Add option to start a a new calculation

--- calculator.py
+++ calculator.py
@@ -21,23 +21,27 @@ operations = {
   "/": divide,
   }
 
-num1 = float(input("What's the first number?: "))
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = float(input("What's the second number?: "))
-answer = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-# Continue program until user types 'n'
-continue_program = "y"
-while continue_program == "y":
-  continue_program = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit. ")
-  if continue_program == "y":
-    num1 = answer
-    operation_symbol = input("Pick an operation: ")
-    num2 = float(input("What's the next number?: "))
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = float(input("What's the second number?: "))
     answer = operations[operation_symbol](num1, num2)
     print(f"{num1} {operation_symbol} {num2} = {answer}")
-  else:
-    continue_program = "no"
+
+    # Continue program until user types 'n'
+    continue_program = "y"
+    while continue_program == "y":
+        continue_program = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        if continue_program == "y":
+            num1 = answer
+            operation_symbol = input("Pick an operation: ")
+            num2 = float(input("What's the next number?: "))
+            answer = operations[operation_symbol](num1, num2)
+            print(f"{num1} {operation_symbol} {num2} = {answer}")
+        else:
+            continue_program = "no"
+            calculator()
+
+calculator()
