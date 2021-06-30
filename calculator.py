692d6022b85ef3fa9f13719b92d50ae142fdd3df
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-30 11:29:37 -0400

    Add code to call function from dictionary

--- calculator.py
+++ calculator.py
@@ -26,3 +26,5 @@ num2 = float(input("What's the second number? "))
 for symbol in operations:
   print(symbol)
 operation_symbol = input("Pick an operation from the line above: ")
+answer = operations[operation_symbol](num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
