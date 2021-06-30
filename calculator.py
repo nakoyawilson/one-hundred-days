9f29fcc616e7aec4dd3759e939e7fc1877eb6991
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-30 11:49:04 -0400

    Add code to continue program until user types 'n'

--- calculator.py
+++ calculator.py
@@ -28,3 +28,16 @@ operation_symbol = input("Pick an operation from the line above: ")
 num2 = float(input("What's the second number?: "))
 answer = operations[operation_symbol](num1, num2)
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+# Continue program until user types 'n'
+continue_program = "y"
+while continue_program == "y":
+  continue_program = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit. ")
+  if continue_program == "y":
+    num1 = answer
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    answer = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+  else:
+    continue_program = "no"
