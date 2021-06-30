14edb57ce25d541f4770bc5a712c30a0b09a070f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-30 11:14:12 -0400

    Add a space between n1, and n2

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 # Add
-def add(n1,n2):
+def add(n1, n2):
   return n1 + n2
 
 # Subtract
-def subtract(n1,n2):
+def subtract(n1, n2):
   return n1 - n2
 
 # Multiply
-def multiply(n1,n2):
+def multiply(n1, n2):
   return n1 * n2
 
 # Divide
-def divide(n1,n2):
+def divide(n1, n2):
   return n1 / n2
