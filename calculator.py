2f3aac056a5179d527f6f43aaafceb3ee0590786
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-30 11:20:51 -0400

    Add code to get user inputs

--- calculator.py
+++ calculator.py
@@ -20,3 +20,9 @@ operations = {
   "*": multiply,
   "/": divide,
   }
+
+num1 = float(input("What's the first number? "))
+num2 = float(input("What's the second number? "))
+for symbol in operations:
+  print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
