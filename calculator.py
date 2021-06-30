0a8f1765e3a61c773aa8f224451dd1d01c4af450
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-30 11:10:44 -0400

    Create functions for mathematical operations

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Add
+def add(n1,n2):
+  return n1 + n2
+
+# Subtract
+def subtract(n1,n2):
+  return n1 - n2
+
+# Multiply
+def multiply(n1,n2):
+  return n1 * n2
+
+# Divide
+def divide(n1,n2):
+  return n1 / n2
