d629de6e5eb1763b821171a3d7d93537011a6b93
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-01 09:19:47 -0400

    Create variables

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 # Our Blackjack House Rules:
 # The deck is unlimited in size.
 # There are no jokers.
@@ -8,3 +10,7 @@
 # The cards in the list have equal probability of being drawn.
 # Cards are not removed from the deck as they are drawn.
 # The computer is the dealer.
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_cards = []
+dealer_cards = []
