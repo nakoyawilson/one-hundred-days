005ab801ac443bb04088556aab10f073294252de
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-04 11:16:39 -0400

    Separate program into different functions

--- higher_lower.py
+++ higher_lower.py
@@ -2,51 +2,71 @@ from higher_lower_art import logo, vs
 from higher_lower_game_data import data
 import random
 
+# Functions
+def choose_entry():
+    """Chooses random entry from game data list"""
+    return random.choice(data)
+
+def assign_answer(choice):
+    """Assigns player's choice to an answer"""
+    if choice == "A":
+        answer = choice_a
+    elif choice == "B":
+        answer = choice_b
+    else:
+        return "Invalid input"
+    return answer
+
+# Compare follower count
+def compare_follower_counts(first_choice, second_choice):
+    """Compares follower counts and returns correct answer"""
+    if first_choice['follower_count'] > second_choice['follower_count']:
+        answer = first_choice
+    else:
+        answer = second_choice
+    return answer
+
+def play_game(first_choice, second_choice):
+    # Print instructions for player.
+    print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}")
+    print(vs)
+    print(f"Against B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}")
+    # Ask who has more followers
+    choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+    return choice
+
 # Print game logo
 print(logo)
 
-# Choose two entries from data list: 'A' and 'B'
-def choose_entry():
-    return random.choice(data)
-choice_a = choose_entry()
-choice_b = choose_entry()
-if choice_b == choice_a:
-    choice_b = choose_entry()
-
-# Print instructions for player.
-print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")
-print(vs)
-print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")
-
-# Ask who has more followers
-player_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-player_answer = ""
-if player_choice == "A":
-    player_answer = choice_a
-elif player_choice == "B":
-    player_answer = choice_b
-else:
-    print("Invalid input")
-print(player_answer)
-
-# Compare follower count
-correct_answer = ""
-if choice_a['follower_count'] > choice_b['follower_count']:
-    correct_answer = choice_a
-else:
-    correct_answer = choice_b
-print(correct_answer)
-
-# Display results
-# If player is correct, add 1 to score
-# Game continues and B becomes A
-# Choose new B
-# If player is wrong, game ends
-score = 0
-if player_answer == correct_answer:
-    score += 1
-    print(f"You're right! Current score: {score}.")
-else:
-    print(f"Sorry, that's wrong. Final score: {score}")
-
-
+play_higher_lower = "y"
+while play_higher_lower == "y":
+    play_higher_lower = input("Do you want to play? Type 'y' or 'n': ").lower()
+    if play_higher_lower == "y":
+        # Choose two entries from data list: 'A' and 'B'
+        choice_a = choose_entry()
+        choice_b = choose_entry()
+        if choice_b == choice_a:
+            choice_b = choose_entry()
+        player_choice = play_game(choice_a, choice_b)
+        correct_answer = compare_follower_counts(choice_a, choice_b)
+        player_answer = assign_answer(player_choice)
+        # Display results. If player is correct, add 1 to score.
+        # Game continues and B becomes A. Choose new B
+        score = 0
+        if player_answer == correct_answer:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+            while player_answer == correct_answer:
+                choice_a = choice_b
+                choice_b = choose_entry()
+                if choice_b == choice_a:
+                    choice_b = choose_entry()
+                player_choice = play_game(choice_a, choice_b)
+                correct_answer = compare_follower_counts(choice_a, choice_b)
+                player_answer = assign_answer(player_choice)
+        # If player is wrong, game ends
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            break
+    else:
+        print("Thanks for playing!")
