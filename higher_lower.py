b426b0d78b931332392de7422a2e5fc22a6fcf15
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-04 09:59:14 -0400

    Add comments outlining steps of game

---
+++ higher_lower.py
@@ -0,0 +1,15 @@
+# Print game logo
+
+# Choose two entries from data dictionary: 'A' and 'B'
+
+# Print instructions to player.
+
+# Ask who has more followers
+
+# Display results
+
+# If player is correct, add 1 to score
+# Game continues and B becomes A
+# Choose new B
+
+# If player is wrong, game ends
