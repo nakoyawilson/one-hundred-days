c6cc647096d4012e66ac7ef59a6a2a5dc00a966b
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-04 10:26:35 -0400

    Add code to compare follower counts of choices

--- higher_lower.py
+++ higher_lower.py
@@ -11,13 +11,29 @@ choice_b = random.choice(data)
 if choice_b == choice_a:
     choice_b = random.choice(data)
 
-# Print instructions to player.
+# Print instructions for player.
 print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")
 print(vs)
 print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")
-answer = input("Who has more followers? Type 'A' or 'B': ")
 
 # Ask who has more followers
+player_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+player_answer = ""
+if player_choice == "A":
+    player_answer = choice_a
+elif player_choice == "B":
+    player_answer = choice_b
+else:
+    print("Invalid input")
+print(player_answer)
+
+# Compare follower count
+correct_answer = ""
+if choice_a['follower_count'] > choice_b['follower_count']:
+    correct_answer = choice_a
+else:
+    correct_answer = choice_b
+print(correct_answer)
 
 # Display results
 
