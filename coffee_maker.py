3d0faec262f829b69806213094848270c699c6f2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-05 10:04:06 -0400

    Add code to prompt user, print report, and end program

--- coffee_maker.py
+++ coffee_maker.py
@@ -29,3 +29,20 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+machine_on = True
+while machine_on:
+    selection = input("What would you like? (espresso/latte/cappuccino): ")
+    if selection == "off":
+        machine_on = False
+    elif selection == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: $")
+    elif selection == "espresso":
+        pass
+    elif selection == "latte":
+        pass
+    elif selection == "cappuccino":
+        pass
