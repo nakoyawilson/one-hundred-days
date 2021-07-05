1b2b36487a6210ea3fb0da1e2f78fc724b7b9e8a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-05 11:56:18 -0400

    Create and call functions to process money and make coffee

--- coffee_maker.py
+++ coffee_maker.py
@@ -47,7 +47,34 @@ def check_resources(drink_selection, machine_resources):
     elif machine_coffee < coffee_needed:
         return "Sorry, there is not enough coffee."
     else:
-        return
+        return True
+
+def process_coins():
+    print("Please insert coins.")
+    quarters = int(input("How many quarters? "))
+    dimes = int(input("How many dimes? "))
+    nickels = int(input("How many nickels? "))
+    pennies = int(input("How many pennies? "))
+    total_coins = quarters * 0.25 + dimes * 0.1 + nickels * 0.05 + pennies * 0.01
+    return total_coins
+
+def sufficient_funds(payment, cost):
+    if payment >= cost:
+        return True
+    else:
+        return False
+
+def make_coffee(drink_selection, machine_resources):
+    water_needed = MENU.get(drink_selection, {}).get("ingredients", {}).get("water", 0)
+    milk_needed = MENU.get(drink_selection, {}).get("ingredients", {}).get("milk", 0)
+    coffee_needed = MENU.get(drink_selection, {}).get("ingredients", {}).get("coffee", 0)
+    machine_water = machine_resources.get("water", 0)
+    machine_milk = machine_resources.get("milk", 0)
+    machine_coffee = machine_resources.get("coffee", 0)
+    water_remaining = machine_water - water_needed
+    milk_remaining = machine_milk - milk_needed
+    coffee_remaining = machine_coffee - coffee_needed
+    return water_remaining, milk_remaining, coffee_remaining
 
 machine_on = True
 profit = 0
@@ -59,6 +86,24 @@ while machine_on:
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: $ ${profit:.2f}")
+        print(f"Money: ${profit:.2f}")
     elif selection == "espresso" or selection == "latte" or selection == "cappuccino":
         resources_sufficient = check_resources(selection, resources)
+        if resources_sufficient:
+            user_payment = process_coins()
+            drink_cost = MENU.get(selection, {}).get("cost", 0)
+            sufficient_coins = sufficient_funds(user_payment, drink_cost)
+            if sufficient_coins:
+                if user_payment == drink_cost:
+                    profit += user_payment
+                else:
+                    profit += drink_cost
+                    user_change = user_payment - drink_cost
+                    print(f"Here is ${user_change:.2f} dollars in change.")
+                    new_water, new_milk, new_coffee = make_coffee(selection, resources)
+                    resources["water"] = new_water
+                    resources["milk"] = new_milk
+                    resources["coffee"] = new_coffee
+                    print(f"Here is your {selection}. Enjoy!")
+            else:
+                print(f"Sorry, that's not enough money. Money refunded")
