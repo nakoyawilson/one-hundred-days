220ff2e6bd65a63ba0dfc53e40bbf0d892199517
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-06 08:46:05 -0400

    Create menu object and propmpt user to input drink selection

--- oop-coffee-machine/main.py
+++ oop-coffee-machine/main.py
@@ -1,3 +1,8 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+the_menu = Menu()
+menu_items = the_menu.get_items()[:-1]
+
+user_selection = input(f"What would you like? ({menu_items}): ")
