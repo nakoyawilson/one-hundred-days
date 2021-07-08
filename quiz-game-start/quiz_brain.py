4d370e036a532c179eaed9a8efc877c7c6d8894e
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-07 21:39:44 -0400

    Create code to check answer and keep score

--- quiz-game-start/quiz_brain.py
+++ quiz-game-start/quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         if len(self.question_list) > self.question_number:
@@ -11,7 +12,17 @@ class QuizBrain:
             return False
 
     def next_question(self):
-        current_question = self.question_list[self.question_number].text
+        current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question} (True/False)?: ").lower()
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ").lower()
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"The current score is: {self.score}/{self.question_number}\n\n")
 
