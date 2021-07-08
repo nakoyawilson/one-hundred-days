16922ccad57a74e4c026ecf6d3e74772eec67eba
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-07 21:44:32 -0400

    Add code to print final score

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -12,3 +12,5 @@ for item in question_data:
 quiz_brain = QuizBrain(question_bank)
 while quiz_brain.still_has_questions():
     quiz_brain.next_question()
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz_brain.score}/{len(quiz_brain.question_list)}")
