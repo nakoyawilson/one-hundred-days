d254687885c64f31e3c21f284f03fa6944f0a6ef
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-08 09:25:57 -0400

    Create colour_list

--- hirst-painting/main.py
+++ hirst-painting/main.py
@@ -1,13 +1,18 @@
-import colorgram
+# import colorgram
+#
+# # Extract 10 colours from image
+# extracted_colours = colorgram.extract("image.jpeg", 10)
+#
+# colours = []
+# for index, extract in enumerate(extracted_colours):
+#     colour_extract = extracted_colours[index].rgb
+#     r, g, b = colour_extract
+#     colour = (r, g, b)
+#     colours.append(colour)
+# print(colours)
 
-# Extract 9 colours from image
-extracted_colours = colorgram.extract("image.jpeg", 9)
+# # Remove white from list
+# colours.pop(0)
+# print(colours)
 
-colours = []
-for index, extract in enumerate(extracted_colours):
-    colour_extract = extracted_colours[index].rgb
-    r, g, b = colour_extract
-    colour = (r, g, b)
-    colours.append(colour)
-
-print(colours)
+colour_list = [(115, 160, 192), (134, 46, 112), (242, 243, 246), (103, 34, 79), (200, 121, 179), (163, 62, 43), (18, 25, 48), (122, 121, 127), (244, 238, 242)]
