b5bbad1d56585d0bb09137ac5e9ca6d683d29012
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-10 13:30:22 -0400

    Create starting snake

--- snake-game/main.py
+++ snake-game/main.py
@@ -5,5 +5,15 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
 
+# Create starting snake
+starting_x = 0
+starting_y = 0
+snake_length = 0
+while snake_length < 3:
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.goto(starting_x, starting_y)
+    snake_length += 1
+    starting_x -= 20
 
 screen.exitonclick()
