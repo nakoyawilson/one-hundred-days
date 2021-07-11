efe850b2929150ab3887e64a7e7809d313af0d02
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-11 00:36:41 -0400

    Refactor code and create game_over method

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -11,12 +11,15 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.color("white")
-        self.setposition(0, 270)
+        self.goto(0, 270)
         self.display_score()
 
     def display_score(self):
-        self.write(f"Score: {self.score}", True, align=ALIGNMENT, font=FONT)
-        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def update_score(self):
         self.score += 1
