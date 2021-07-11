bdd022d4ff09dda289bd0328b6eb015e8482e27d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-11 01:16:34 -0400

    Refactor code to detect collision with tail

--- snake-game/main.py
+++ snake-game/main.py
@@ -38,10 +38,8 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with tail
-    for segment in snake.snake_segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.snake_segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
