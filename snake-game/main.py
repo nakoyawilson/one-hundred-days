1f19432f41f217b32ff2e198d7dab65f736c44da
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-11 00:13:09 -0400

    Create scoreboard object and add code to update score when there's a collision

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,6 +29,8 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.update_score()
+        scoreboard.display_score()
 
 
 screen.exitonclick()
