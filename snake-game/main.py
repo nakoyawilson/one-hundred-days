b8cb68067b8d4960b3753b9d6a6bfc2be1089427
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-11 01:06:24 -0400

    Add code detect collision with tail

--- snake-game/main.py
+++ snake-game/main.py
@@ -34,7 +34,15 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
         game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.snake_segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
