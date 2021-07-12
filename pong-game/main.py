a250460e561fda44b5c4cdb6d6620f000eebb66b
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-12 16:11:18 -0400

    Create move functionality for left paddle

--- pong-game/main.py
+++ pong-game/main.py
@@ -5,6 +5,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("A Game of Pong")
+screen.tracer(0)
 
 right_paddle = Paddle(x_start=350, y_start=0)
 left_paddle = Paddle(x_start=-350, y_start=0)
@@ -12,6 +13,12 @@ left_paddle = Paddle(x_start=-350, y_start=0)
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+
+game_on = True
+while game_on:
+    screen.update()
 
 
 screen.exitonclick()
