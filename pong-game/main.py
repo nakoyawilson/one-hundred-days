027f97508eae4eec6a962840086076d8bc617ea9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-12 15:50:33 -0400

    Create left and right instances of paddle

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,10 +1,17 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("A Game of Pong")
-screen.tracer(0)
+
+right_paddle = Paddle(x_start=350, y_start=0)
+left_paddle = Paddle(x_start=-350, y_start=0)
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
 
 
 screen.exitonclick()
