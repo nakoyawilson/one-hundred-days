3e04b102e4850ec5170965bff31ca2c9a6dbc657
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-12 19:25:09 -0400

    Refactor code

--- pong-game/paddle.py
+++ pong-game/paddle.py
@@ -3,24 +3,21 @@ from turtle import Turtle
 MOVE_DISTANCE = 20
 UP = 90
 
-class Paddle:
+class Paddle(Turtle):
 
     def __init__(self, x_start, y_start):
+        super().__init__()
         self.starting_x = x_start
         self.starting_y = y_start
-        self.paddle = self.create_paddle(self.starting_x, self.starting_y)
-
-    def create_paddle(self, starting_x, starting_y):
-        paddle = Turtle(shape="square")
-        paddle.color("white")
-        paddle.shapesize(stretch_len=5, stretch_wid=1)
-        paddle.penup()
-        paddle.setheading(UP)
-        paddle.goto(starting_x, starting_y)
-        return paddle
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=5, stretch_wid=1)
+        self.penup()
+        self.setheading(UP)
+        self.goto(self.starting_x, self.starting_y)
 
     def up(self):
-        self.paddle.forward(MOVE_DISTANCE)
+        self.forward(MOVE_DISTANCE)
 
     def down(self):
-        self.paddle.backward(MOVE_DISTANCE)
+        self.backward(MOVE_DISTANCE)
