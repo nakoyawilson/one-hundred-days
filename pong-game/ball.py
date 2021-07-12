183b2ca15c31aae13c8b571d520a4eece58a662d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-12 19:25:50 -0400

    Create opposite_start method

--- pong-game/ball.py
+++ pong-game/ball.py
@@ -17,3 +17,10 @@ class Ball(Turtle):
 
     def wall_bounce(self):
         self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
+
+    def opposite_start(self):
+        self.y_move *= -1
+        self.x_move *= -1
