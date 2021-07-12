196a50cdb1531725ee21bc468fdd37588d8fe993
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-12 07:18:46 -0400

    Set up screen

---
+++ pong-game/main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("A Game of Pong")
+screen.tracer(0)
+
+
+screen.exitonclick()
