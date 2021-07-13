a6c7eeb0cc8d57f949e2f004a9a9fe695319cdb2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-13 08:31:52 -0400

    Create CarManager class and create_car, move and change speed methods

--- turtle-crossing/car_manager.py
+++ turtle-crossing/car_manager.py
@@ -1,7 +1,29 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+STARTING_X = 300
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.car_speed = 0.1
+
+    def create_car(self):
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.penup()
+        self.color(random.choice(COLORS))
+        self.setheading(180)
+        starting_y = random.randrange(-260, 260, 20)
+        self.goto(STARTING_X, starting_y)
+
+    def move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
+
+    def change_speed(self):
+        self.car_speed *= 0.9
