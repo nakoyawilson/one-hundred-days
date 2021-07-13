062c83e0f9e02bf7e963713bbea7187587f62971
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-13 08:33:16 -0400

    Create a car object and functionality to create next car when car moves off screen

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -9,13 +9,22 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+cars = CarManager()
+cars.create_car()
 
 screen.listen()
 screen.onkey(player.up, "Up")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(cars.car_speed)
     screen.update()
+
+    cars.move()
+
     if player.ycor() > FINISH_LINE_Y:
         player.level_up()
+        cars.change_speed()
+
+    if cars.xcor() < -300:
+        cars.create_car()
