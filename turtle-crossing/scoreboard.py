f6822f5751ba9f68720397b22799388b7d31df34
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-13 09:04:55 -0400

    Create Scoreboard class with increase_level, update_level and game_over methods

--- turtle-crossing/scoreboard.py
+++ turtle-crossing/scoreboard.py
@@ -1,5 +1,24 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+
+    def increase_level(self):
+        self.level += 1
+
+    def update_level(self):
+        self.clear()
+        self.goto(-280, 260)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center", font=FONT)
