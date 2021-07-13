dd83c5a733668674bd596b681acef9c01e15e883
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-13 09:07:05 -0400

    Create scoreboard object and add update level and game over functionality

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 player = Player()
 cars = CarManager()
 cars.create_car()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.up, "Up")
@@ -20,11 +21,21 @@ while game_is_on:
     time.sleep(cars.car_speed)
     screen.update()
 
+    scoreboard.update_level()
     cars.move()
 
     if player.ycor() > FINISH_LINE_Y:
         player.level_up()
         cars.change_speed()
+        scoreboard.increase_level()
 
     if cars.xcor() < -300:
         cars.create_car()
+
+    # Detect collision with car
+    if player.distance(cars) < 25:
+        game_is_on = False
+        scoreboard.game_over()
+
+
+screen.exitonclick()
