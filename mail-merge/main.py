f00ea534163f71ff4767678b52c7e2c53c82c58f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-14 09:38:33 -0400

    Add code to create a new letter using starting_letter.txt

--- mail-merge/main.py
+++ mail-merge/main.py
@@ -1,8 +1,25 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+# Create a letter using starting_letter.txt for each name in invited_names.txt
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+# Create list of names
+with open("Input/Names/invited_names.txt") as data:
+    names_list = data.readlines()
+new_names = []
+for name in names_list:
+    new_name = name.strip("\n")
+    new_names.append(new_name)
+
+# Get letter template
+with open("Input/Letters/starting_letter.txt") as template:
+    letter_template = template.read()
+
+for name in new_names:
+    # Replace the [name] placeholder with the actual name
+    new_letter = letter_template.replace("[name]", name)
+    # Save the letters in the folder "ReadyToSend"
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as letter:
+        letter.write(new_letter)
+
