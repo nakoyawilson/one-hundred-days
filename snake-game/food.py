6d0cb12eaf79c505f32fcf0de588d1718af3f087
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-14 07:50:56 -0400

    Reduce area where food generates

--- snake-game/food.py
+++ snake-game/food.py
@@ -13,6 +13,6 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.randint(-270, 280)
+        random_y = random.randint(-270, 280)
         self.goto(random_x, random_y)
