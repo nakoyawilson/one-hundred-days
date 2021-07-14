1cdf34d54caf304903b5abf3afe70518178634ef
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-14 08:02:24 -0400

    Add high score functionality

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -3,11 +3,15 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Menlo", 20, "normal")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            high_score = file.read()
+        self.high_score = int(high_score)
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -15,7 +19,7 @@ class Scoreboard(Turtle):
         self.display_score()
 
     def display_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}    High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
@@ -26,3 +30,11 @@ class Scoreboard(Turtle):
         self.clear()
         self.display_score()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("data.txt", "w") as file:
+            file.write(f"{self.high_score}")
+        self.score = 0
+        self.clear()
+        self.display_score()
