983892e08e07f00228cfd34fa3998ea3e1943a35
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-15 16:32:35 -0400

    Write correct guess onto the map

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -6,6 +6,9 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+answer = turtle.Turtle()
+answer.penup()
+answer.hideturtle()
 
 states_data = pandas.read_csv("50_states.csv")
 states_list = states_data.state.to_list()
@@ -13,6 +16,14 @@ states_list = states_data.state.to_list()
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
 
 if answer_state in states_list:
-    print("Correct")
+    state = states_data[states_data.state == answer_state]
+    x_coordinate = int(state.x.to_string(index=False))
+    y_coordinate = int(state.y.to_string(index=False))
+    print(x_coordinate)
+    print(y_coordinate)
+    answer.goto(x_coordinate, y_coordinate)
+    answer.write(f"{answer_state}", align="center", font=("Menlo", 10, "normal"))
 else:
-    print("Incorrect")
+    pass
+
+screen.exitonclick()
