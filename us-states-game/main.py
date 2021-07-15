bda2e70e25f5429a53e220ee20a4e29e4e460dfe
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-15 16:35:26 -0400

    Remove print statements and use loop to keep guessing

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -13,17 +13,16 @@ answer.hideturtle()
 states_data = pandas.read_csv("50_states.csv")
 states_list = states_data.state.to_list()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
 
-if answer_state in states_list:
-    state = states_data[states_data.state == answer_state]
-    x_coordinate = int(state.x.to_string(index=False))
-    y_coordinate = int(state.y.to_string(index=False))
-    print(x_coordinate)
-    print(y_coordinate)
-    answer.goto(x_coordinate, y_coordinate)
-    answer.write(f"{answer_state}", align="center", font=("Menlo", 10, "normal"))
-else:
-    pass
+
+game_is_on = True
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
+    if answer_state in states_list:
+        state = states_data[states_data.state == answer_state]
+        x_coordinate = int(state.x.to_string(index=False))
+        y_coordinate = int(state.y.to_string(index=False))
+        answer.goto(x_coordinate, y_coordinate)
+        answer.write(f"{answer_state}", align="center", font=("Menlo", 10, "normal"))
 
 screen.exitonclick()
