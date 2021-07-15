5f63ecde7a8a9de3fa0552116cc926b22e87bf65
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-15 17:34:48 -0400

    Refactor code with respect to score variable

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -15,8 +15,8 @@ states_list = states_data.state.to_list()
 
 game_is_on = True
 correct_guesses = []
+score = 0
 while game_is_on:
-    score = len(correct_guesses)
     answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="Name a state:").title()
     if answer_state in states_list:
         if answer_state in correct_guesses:
@@ -28,9 +28,10 @@ while game_is_on:
             answer.goto(x_coordinate, y_coordinate)
             answer.write(f"{answer_state}", align="center", font=("Menlo", 10, "normal"))
             correct_guesses.append(answer_state)
-    if score == 49:
+    score = len(correct_guesses)
+    if score == 50:
         answer.goto(0, 0)
-        answer.write("Congratulations! You named all 50 states", align="center", font=("Menlo", 20, "normal"))
+        answer.write("Congratulations! You named all 50 states.", align="center", font=("Menlo", 22, "normal"))
         game_is_on = False
 
 screen.exitonclick()
