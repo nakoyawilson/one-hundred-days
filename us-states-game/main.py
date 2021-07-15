06acbbc56d0b42c6af7adccb23ef302e7b18c5f4
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-15 16:07:44 -0400

    Check if player's guess is among the 50 states

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,6 +7,12 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+states_data = pandas.read_csv("50_states.csv")
+states_list = states_data.state.to_list()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?")
-print(answer_state)
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
+
+if answer_state in states_list:
+    print("Correct")
+else:
+    print("Incorrect")
