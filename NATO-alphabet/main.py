8c1a123a5d6b8229e6c40fa0a685f066666df009
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-16 16:49:12 -0400

    Delete unnecessary code

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,24 +1,4 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 # Create a dictionary for NATO alphabet:
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
