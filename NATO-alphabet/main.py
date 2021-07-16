3e6d2a36190a4b8b972fee034a67a810cb82040b
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-16 16:53:09 -0400

    Refactor code

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -2,8 +2,7 @@ import pandas
 
 # Create a dictionary for NATO alphabet:
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dataframe = pandas.DataFrame(nato_alphabet)
-nato_dict = {row.letter:row.code for (index, row) in nato_dataframe.iterrows()}
+nato_dict = {row.letter:row.code for (index, row) in nato_alphabet.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
 user_input = input("Enter a word: ").upper()
