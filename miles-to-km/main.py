33b24ffcb06fc315d791b72a8ff1e710d4924c90
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-17 21:47:12 -0400

    Add blank lines

--- miles-to-km/main.py
+++ miles-to-km/main.py
@@ -13,14 +13,19 @@ window.config(padx=20, pady=20)
 
 miles_input = Entry(width=10)
 miles_input.grid(column=1, row=0)
+
 miles_unit = Label(text="Miles")
 miles_unit.grid(column=2, row=0)
+
 equal = Label(text="is equal to")
 equal.grid(column=0, row=1)
+
 km_output = Label(text="0")
 km_output.grid(column=1, row=1)
+
 km_unit = Label(text="Km")
 km_unit.grid(column=2, row=1)
+
 calculate = Button(text="Calculate", command=miles_to_km)
 calculate.grid(column=1, row=2)
 
