745bea2750accc7639d77ca5c163939863d8a20e
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-18 09:38:43 -0400

    Add necessary widgets

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -20,10 +20,22 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmarks = Label(text="️✔", fg=GREEN, bg=YELLOW)
+checkmarks.grid(column=1, row=3)
 
 window.mainloop()
