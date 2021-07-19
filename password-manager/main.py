074cc22c5edea09007a767a3359dcb6e83ef18db
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-19 07:34:19 -0400

    Create the user interface using grid layout

--- password-manager/main.py
+++ password-manager/main.py
@@ -12,6 +12,30 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(column=0, row=2)
+
+username_entry = Entry(width=35)
+username_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
