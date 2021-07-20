0fbf5237e4007b3298a9f9b3875d5dc67a04fa12
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-20 08:42:01 -0400

    Add code to try and except KeyError

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -5,7 +5,15 @@ nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in nato_alphabet.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-phonetic_code = [nato_dict[letter] for letter in user_input]
-print(phonetic_code)
+word_not_submitted = True
+while word_not_submitted:
+    user_input = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(phonetic_code)
+        word_not_submitted = False
+
 
