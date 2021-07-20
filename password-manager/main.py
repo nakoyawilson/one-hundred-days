9fd2de2376442268f59040f15329c785391f5e70
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-20 09:39:19 -0400

    Add code to try and except for FileNotFoundError

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -30,21 +32,40 @@ def generate_password():
     # Copy password to clipboard
     pyperclip.copy(randomised_password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showwarning(title="Oops", message="Please don't leave any fields empty!")
 
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered:\nUsername: {username}\nPassword: {password}\nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data:
-                data.write(f"{website} | {username} | {password}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                # Read old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            # Create new file
+            with open("data.json", "w") as data_file:
+                # Write new data
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Update old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Save updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, "end")
             password_entry.delete(0, "end")
 
