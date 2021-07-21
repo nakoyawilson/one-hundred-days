64e97d1b843dac0148ed6dd1129182ee01403499
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-20 21:37:05 -0400

    Create the User Interface with Tkinter

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,2 +1,29 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+card_back_img = PhotoImage(file="images/card_back.png")
+card_front_img = PhotoImage(file="images/card_front.png")
+right_img = PhotoImage(file="images/right.png")
+wrong_img = PhotoImage(file="images/wrong.png")
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(400, 263, image=card_front_img)
+title_text = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+right_button = Button(image=right_img, highlightthickness=0)
+right_button.grid(column=1, row=1)
+
+window.mainloop()
+
 
