fd69fab0834300ba385b3e84a7f3cc522c8e42d3
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-22 09:24:20 -0400

    Extract and format data from text file using regex

---
+++ birthday-wisher/extract_birthdays.py
@@ -0,0 +1,14 @@
+import re
+
+with open("acnh_birthdays.txt") as data_file:
+    birthdays = data_file.readlines()
+
+for birthday_data in birthdays:
+    birthday_regex = re.compile(r'(\d+) (\d+): (\w+)')
+    match_object = birthday_regex.search(birthday_data)
+    name = match_object[3]
+    month = match_object[1]
+    day = match_object[2]
+    data = f"{name},nwilsonautomated@yahoo.com,2001,{month},{day}\n"
+    with open("birthdays.csv", "a") as csv_file:
+        csv_file.write(data)
