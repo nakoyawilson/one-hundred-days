a9dd607034f77c847fbb12a5e13366288172e080
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-22 07:31:55 -0400

    Add code to send the letter to recipient's email address

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,12 +1,10 @@
-##################### Extra Hard Starting Project ######################
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
 import pandas
 import datetime as dt
 import random
+import smtplib
+
+MY_EMAIL = "example_email@gmail.com"
+MY_PASSWORD = "ExamplePassword123"
 
 birthday_data = pandas.read_csv("birthdays.csv")
 
@@ -17,16 +15,20 @@ today_day = date_today.day
 letter_templates = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
 
 for row in birthday_data.itertuples(index=False, name="Person"):
-    name = row.name
-    email = row.email
+    recipient_name = row.name
+    recipient_email = row.email
     birthday_month = row.month
     birthday_day = row.day
     if birthday_month == today_month and birthday_day == today_day:
         letter_template = random.choice(letter_templates)
         with open(letter_template) as template:
             letter = template.read()
-            email_message = letter.replace("[NAME]", name)
-            print(email_message)
+            email_message = letter.replace("[NAME]", recipient_name)
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+                connection.sendmail(from_addr=MY_EMAIL, to_addrs=recipient_email,
+                                    msg=f"Subject: Happy Birthday, {recipient_name}!\n\n{email_message}")
 
 
 
