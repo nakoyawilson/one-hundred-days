e5b4d78e91f7560080353a54ecc67e8adee5cbe9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-22 07:14:44 -0400

    Check if today matches a birthday in the birthdays.csv

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,12 +1,27 @@
 ##################### Extra Hard Starting Project ######################
 
-# 1. Update the birthdays.csv
-
 # 2. Check if today matches a birthday in the birthdays.csv
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
+
+import pandas
+import datetime as dt
+
+birthday_data = pandas.read_csv("birthdays.csv")
+
+date_today = dt.datetime.now()
+today_month = date_today.month
+today_day = date_today.day
+
+for row in birthday_data.itertuples(index=False, name="Person"):
+    name = row.name
+    email = row.email
+    birthday_month = row.month
+    birthday_day = row.day
+    if birthday_month == today_month and birthday_day == today_day:
+        print(name)
 
 
 
