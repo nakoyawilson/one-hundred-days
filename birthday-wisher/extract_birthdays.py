d150541927be62b81fcfe8767ca46be7ad999463
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-22 12:34:11 -0400

    Add code to account for if birthdays.csv does not exist

--- birthday-wisher/extract_birthdays.py
+++ birthday-wisher/extract_birthdays.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 with open("acnh_birthdays.txt") as data_file:
     birthdays = data_file.readlines()
@@ -10,5 +11,9 @@ for birthday_data in birthdays:
     month = match_object[1]
     day = match_object[2]
     data = f"{name},nwilsonautomated@yahoo.com,2001,{month},{day}\n"
-    with open("birthdays.csv", "a") as csv_file:
-        csv_file.write(data)
+    if not os.path.isfile("birthdays.csv"):
+        with open("birthdays.csv", "w") as csv_file:
+            csv_file.write(f"name,email,year,month,day\n{data}")
+    else:
+        with open("birthdays.csv", "a") as csv_file:
+            csv_file.write(data)
