044c7364e4916eb72c32daca415e98fc738977ed
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-23 07:00:16 -0400

    Change latitude and longitude coordinates

--- iss-overhead/main.py
+++ iss-overhead/main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 10.597344
+MY_LONG = -61.339448
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
