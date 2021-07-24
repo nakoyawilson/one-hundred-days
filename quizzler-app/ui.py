2a43ee66f3a0be3805a00564bef991293d3230aa
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-24 06:58:42 -0400

    Create get_next_question method

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ("Arial", 20, "italic")
@@ -6,14 +7,16 @@ FONT = ("Arial", 20, "italic")
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
         self.score_text = Label(text="Score: 0", bg=THEME_COLOR, fg="#fff")
         self.score_text.grid(column=1, row=0)
         self.canvas = Canvas(width=300, height=250)
-        self.question_text = self.canvas.create_text(150, 125, text="Quiz questions here", font=FONT, fill=THEME_COLOR)
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Quiz questions here", font=FONT,
+                                                     fill=THEME_COLOR)
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
         true_img = PhotoImage(file="images/true.png")
         self.true_button = Button(image=true_img, highlightthickness=0)
@@ -21,4 +24,10 @@ class QuizInterface:
         false_img = PhotoImage(file="images/false.png")
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
