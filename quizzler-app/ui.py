1fa7da8ddb6ad4a339fefcea4226d96a4114bf70
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-24 06:49:48 -0400

    Create the QuizInterface class

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,2 +1,24 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+FONT = ("Arial", 20, "italic")
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+        self.score_text = Label(text="Score: 0", bg=THEME_COLOR, fg="#fff")
+        self.score_text.grid(column=1, row=0)
+        self.canvas = Canvas(width=300, height=250)
+        self.question_text = self.canvas.create_text(150, 125, text="Quiz questions here", font=FONT, fill=THEME_COLOR)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+        self.window.mainloop()
