66b6afef085466a1387a9d87c543763a3066d195
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-26 13:56:42 -0400

    Add initial call to select_word()

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -58,8 +58,8 @@ wrong_img = PhotoImage(file="images/wrong.png")
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flashcard = canvas.create_image(400, 263, image=card_front_img)
-title_text = canvas.create_text(400, 150, text="Language", font=("Arial", 40, "italic"))
-word_text = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+title_text = canvas.create_text(400, 150, font=("Arial", 40, "italic"))
+word_text = canvas.create_text(400, 263, font=("Arial", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_button = Button(image=wrong_img, highlightthickness=0, command=select_word)
@@ -67,6 +67,8 @@ wrong_button.grid(column=0, row=1)
 
 right_button = Button(image=right_img, highlightthickness=0, command=lambda: [save_progress(), select_word()])
 right_button.grid(column=1, row=1)
+
+select_word()
 
 window.mainloop()
 
