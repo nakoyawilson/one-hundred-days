895a87eb582775c04040987046a0dd34576a01c5
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-26 08:23:04 -0400

    Complete STEP 1: Use https://www.alphavantage.co

--- stock-news/main.py
+++ stock-news/main.py
@@ -1,8 +1,58 @@
+import requests
+import os
+from datetime import datetime, timedelta
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+
+def get_yesterday(num_days):
+    date_today = datetime.now()
+    delta = timedelta(days=num_days)
+    return date_today - delta
+
+
+def get_day_before_yesterday(date_yesterday, num_days):
+    delta = timedelta(days=num_days)
+    return date_yesterday - delta
+
+
+# Get yesterday's date
+if datetime.now().weekday() == 0:
+    yesterday = get_yesterday(3)
+elif datetime.now().weekday() == 6:
+    yesterday = get_yesterday(2)
+else:
+    yesterday = get_yesterday(1)
+# Get day before yesterday's date
+if yesterday.weekday() == 0:
+    day_before = get_day_before_yesterday(yesterday, 3)
+else:
+    day_before = get_day_before_yesterday(yesterday, 1)
+yesterday = str(yesterday.date())
+day_before = str(day_before.date())
+
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+alpha_vantage_api_key = os.environ["AV_API_KEY"]
+alpha_vantage_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "outputsize": "compact",
+    "apikey": alpha_vantage_api_key,
+}
+
+av_response = requests.get("https://www.alphavantage.co/query", params=alpha_vantage_params)
+av_response.raise_for_status()
+stock_data = av_response.json()
+yesterday_price = float(stock_data["Time Series (Daily)"][yesterday]["4. close"])
+day_before_price = float(stock_data["Time Series (Daily)"][day_before]["4. close"])
+price_difference = yesterday_price - day_before_price
+percentage = price_difference/yesterday_price * 100
+if percentage >= 5 or percentage <= -5:
+    print("Get News")
+else:
+    print("Insignificant change in price")
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
