cfa9a8efed076d1a7944a58a09fd91c4a56f081f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-28 03:37:00 -0400

    Save data into Google Sheets

--- exercise-tracking/main.py
+++ exercise-tracking/main.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from datetime import datetime
+import json
 
 APP_ID = os.environ["APP_ID"]
 API_KEY = os.environ["API_KEY"]
@@ -9,6 +11,7 @@ exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
+    "Content-Type": "application/json",
 }
 
 user_input = input("Which exercises did you do today? ")
@@ -17,5 +20,33 @@ query_data = {
     "query": user_input,
 }
 
-response = requests.post(exercise_endpoint, headers=headers, data=query_data)
-print(response.text)
+query_json_data = json.dumps(query_data)
+
+nutritionix_response = requests.post(exercise_endpoint, headers=headers, data=query_json_data)
+exercise_data = nutritionix_response.json()
+
+sheety_post_endpoint = "https://api.sheety.co/9ff690a3187dd8292fa7a40986a8586d/workoutTracking/workouts"
+
+today = datetime.now()
+current_date = today.strftime("%d/%m/%Y")
+current_time = today.strftime("%H:%M:%S %p")
+
+sheety_headers = {
+    "Content-Type": "application/json",
+}
+
+for exercise in exercise_data["exercises"]:
+    exercise_name = exercise["name"]
+    exercise_duration = exercise["duration_min"]
+    calories_burned = exercise["nf_calories"]
+    sheety_data = {
+        "workout": {
+            "date": current_date,
+            "time": current_time,
+            "exercise": exercise_name.title(),
+            "duration": round(exercise_duration),
+            "calories": round(calories_burned),
+        }
+    }
+    sheety_json_data = json.dumps(sheety_data)
+    sheety_response = requests.post(sheety_post_endpoint, data=sheety_json_data, headers=sheety_headers)
