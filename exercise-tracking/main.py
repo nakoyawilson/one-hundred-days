da479088a0cc4292c6c9d85010de887a88ba7ec5
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-28 01:32:01 -0400

    Get exercise stats with natural language queries

--- exercise-tracking/main.py
+++ exercise-tracking/main.py
@@ -1,4 +1,21 @@
 import os
+import requests
 
 APP_ID = os.environ["APP_ID"]
 API_KEY = os.environ["API_KEY"]
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+user_input = input("Which exercises did you do today? ")
+
+query_data = {
+    "query": user_input,
+}
+
+response = requests.post(exercise_endpoint, headers=headers, data=query_data)
+print(response.text)
