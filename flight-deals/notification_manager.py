e60221f777feab73c7fe2cfcb92a7b6a7ca12d05
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-28 17:21:34 -0400

    Import os and twilio modules and create variables

--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,12 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+from_phone_num = os.environ["FROM_PHONE_NUMBER"]
+to_phone_num = os.environ["TO_PHONE_NUMBER"]
+
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     pass
