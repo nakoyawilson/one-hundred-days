5e7309e7898bfb1a18da2d0e48db9753bcaef17f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-29 12:35:03 -0400

    Add functionality to update Google Sheet, search for cheap flights and send alert if prices below metric

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,1 +1,37 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_data import FlightData
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+from datetime import datetime, timedelta
+
+DEPARTURE_LIST = ["London"]
+
+data_manager = DataManager()
+flight_data = FlightData()
+notification_manager = NotificationManager()
+
+# Get list of cities
+cities = data_manager.city_list
+
+# Get list of IATA codes
+city_codes = flight_data.find_iata_codes(city_names=cities)
+
+# Add IATA codes to Google Sheet
+data_manager.add_iata_code(iata_codes=city_codes)
+
+# Get updated Google Sheet data
+updated_data = data_manager.get_data()
+
+# Get departure location IATA code
+departure_location = flight_data.find_iata_codes(city_names=DEPARTURE_LIST)
+departure_code = departure_location[0]
+
+# Use updated data to search for flights
+tomorrow = (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y")
+six_months_later = (datetime.now() + timedelta(days=180)).strftime("%d/%m/%Y")
+flight_search = FlightSearch()
+cheap_flights = flight_search.search_for_flights(sheet_data=updated_data, departure_iata=departure_code, from_date=tomorrow,
+                                           to_date=six_months_later)
+
+for cheap_flight in cheap_flights:
+    notification_manager.send_sms(sms_body=cheap_flight)
