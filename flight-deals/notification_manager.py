38c022686339172700e120d4d7fac2719c6e2703
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-29 12:33:38 -0400

    Create the NotificationManager class

--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,12 +1,21 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-from_phone_num = os.environ["FROM_PHONE_NUMBER"]
-to_phone_num = os.environ["TO_PHONE_NUMBER"]
-
-
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+        self.AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+        self.FROM_PHONE_NUMBER = os.environ["FROM_PHONE_NUMBER"]
+        self.TO_PHONE_NUMBER = os.environ["TO_PHONE_NUMBER"]
+
+    def send_sms(self, sms_body):
+        client = Client(self.ACCOUNT_SID, self.AUTH_TOKEN)
+        message = client.messages \
+            .create(
+            body= sms_body,
+            from_= self.FROM_PHONE_NUMBER,
+            to= self.TO_PHONE_NUMBER,
+        )
+        print(message.status)
