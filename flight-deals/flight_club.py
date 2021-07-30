9eecb7897a1c6d33e1e46487a7381628fb4aa160
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-30 08:15:01 -0400

    Collect user information and post to Google sheet

---
+++ flight-deals/flight_club.py
@@ -0,0 +1,32 @@
+import os
+import requests
+
+SHEETY_AUTH = os.environ["SHEETY_AUTH"]
+SHEETY_POST_ENDPOINT = os.environ["SHEETY_POST_ENDPOINT"]
+
+print(
+    "Welcome to Nakoya's Flight Club.\nWe find the best flight deals and email you."
+)
+first_name = input("What is your first name?\n")
+last_name = input("What is your last name?\n")
+emails_do_not_match = True
+while emails_do_not_match:
+    email = input("What is your email?\n")
+    validate_email = input("Type your email again.\n")
+    if email == validate_email:
+        headers = {
+            "Authorization": SHEETY_AUTH,
+        }
+        data = {
+            "user": {
+                "firstName": first_name,
+                "lastName": last_name,
+                "email": email,
+            }
+        }
+        response = requests.post(SHEETY_POST_ENDPOINT, json=data, headers=headers)
+        response.raise_for_status()
+        print("Success! Your email has been added.")
+        emails_dont_match = False
+    else:
+        print("The emails entered don't match. Please try again.")
