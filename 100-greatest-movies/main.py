7720939228008055c142d7bd103909a77c098850
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-03 14:08:57 -0400

    Get list of 100 Greatest Movies

--- 100-greatest-movies/main.py
+++ 100-greatest-movies/main.py
@@ -1,4 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+response.raise_for_status()
+greatest_movies = response.text
 
+soup = BeautifulSoup(greatest_movies, "html.parser")
+script_text = soup.find(name="script", id="__NEXT_DATA__").contents
+data = json.loads(script_text[0])
+movies = [data["props"]["pageProps"]["data"]["getArticleByFurl"]["_layout"][7]["content"]["images"][movie]["titleText"] for movie in range(100)]
+print(movies)
