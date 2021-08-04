5a753ed5332cac5a279d7d4e92524d55cc0f74e1
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-04 13:47:30 -0400

    Use the requests and BeautifulSoup modules to scrape product price

---
+++ amazon-price-tracker/main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+url = "https://www.amazon.com/gp/product/B0762LT49K/ref=ox_sc_saved_title_9?smid=A3686AOLDPXXOD&th=1"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+}
+
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "lxml")
+price = soup.select_one("#priceblock_ourprice").getText()
+print(price)
