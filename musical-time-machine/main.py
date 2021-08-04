3c7a6f7c9502869ba862b7261b63608a4f727320
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-03 23:34:21 -0400

    Use Spotipy module to authentication with Spotify

--- musical-time-machine/main.py
+++ musical-time-machine/main.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+REDIRECT_URI = "http://example.com"
 
 date = input("What date you would like to travel to? Type the date in the format YYYY-MM-DD: ")
 url = "https://www.billboard.com/charts/hot-100/" + date
@@ -9,10 +16,15 @@ response.raise_for_status()
 hot_100_data = response.text
 
 soup = BeautifulSoup(hot_100_data, "html.parser")
-# print(soup.prettify())
 songs = soup.find_all(name="span", class_="chart-element__information__song text--truncate color--primary")
-artistes = soup.find_all(name="span", class_="chart-element__information__artist text--truncate color--secondary")
+artists = soup.find_all(name="span", class_="chart-element__information__artist text--truncate color--secondary")
 song_titles = [song.getText() for song in songs]
-song_artistes = [artiste.getText() for artiste in artistes]
+song_artists = [artist.getText() for artist in artists]
 print(song_titles)
-print(song_artistes)
+print(song_artists)
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope="playlist-modify-private"))
+user_id = sp.current_user()["id"]
