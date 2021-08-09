5ef23a7398fd9429306f03be3de4e853cc4b31d9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-09 18:18:12 -0400

    Remove unused import

--- auto-job-save/main.py
+++ auto-job-save/main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from time import sleep
 import os
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
