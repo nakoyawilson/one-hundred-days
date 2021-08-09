2b4caf0c07edd83c5de6a70ec87f99055ac58fd2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-09 17:06:08 -0400

    Save and follow first 7 jobs and companies

--- auto-job-save/main.py
+++ auto-job-save/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from time import sleep
 import os
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 LINKEDIN_URL = "https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=92000000&keywords=junior%20python%20developer&location=Worldwide"
 LINKEDIN_EMAIL = os.environ["LINKEDIN_EMAIL"]
@@ -9,32 +10,49 @@ LINKEDIN_PASSWORD = os.environ["LINKEDIN_PASSWORD"]
 
 chrome_driver_path = "/Users/nakoya/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
 driver.get(LINKEDIN_URL)
 
 sleep(2)
 sign_in = driver.find_element_by_css_selector(
     "body > div.base-serp-page > header > nav > div > a.nav__button-secondary")
 sign_in.click()
-
 sleep(2)
 email = driver.find_element_by_name("session_key")
 email.send_keys(LINKEDIN_EMAIL)
+sleep(2)
 password = driver.find_element_by_name("session_password")
 password.send_keys(LINKEDIN_PASSWORD)
+sleep(2)
 submit = driver.find_element_by_css_selector("#organic-div > form > div.login__form_action_container > button")
 submit.click()
 
 sleep(2)
-save_button = driver.find_element_by_css_selector(".jobs-save-button")
-save_button.click()
+links = driver.find_elements_by_css_selector(".job-card-container a")
+job_listings = [link for link in links if link.get_attribute("tabindex") == "0"]
 
-sleep(5)
-job_content = driver.find_element_by_css_selector("body > div.application-outlet > div.authentication-outlet > div.job-search-ext > div > div > section.jobs-search__right-rail")
-job_content.click()
-html = driver.find_element_by_tag_name("html")
-html.send_keys(Keys.END)
+for job in job_listings:
+    sleep(1)
+    job.click()
 
-sleep(2)
-follow_button = driver.find_element_by_css_selector(".jobs-company button")
-follow_button.click()
+    sleep(2)
+    save_button = driver.find_element_by_css_selector(".jobs-save-button")
+    save_button.click()
+
+    sleep(1)
+    dismiss_notification = driver.find_element_by_css_selector(".artdeco-toast-item__dismiss")
+    dismiss_notification.click()
+
+    sleep(2)
+    job_content = driver.find_element_by_css_selector("body > div.application-outlet > div.authentication-outlet > div.job-search-ext > div > div > section.jobs-search__right-rail")
+    job_content.click()
+    html = driver.find_element_by_tag_name("html")
+    html.send_keys(Keys.END)
+
+    sleep(2)
+    follow_button = driver.find_element_by_css_selector(".jobs-company button")
+    follow_button.click()
+
+    sleep(1)
+    dismiss_notification = driver.find_element_by_css_selector(".artdeco-toast-item__dismiss")
+    dismiss_notification.click()
+
