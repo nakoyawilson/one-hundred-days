2dd5c211c1caf3939dba4736a60ac9582db7a383
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-09 07:01:53 -0400

    Save job and follow the company that posted the listing

--- auto-job-save/main.py
+++ auto-job-save/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from time import sleep
 import os
+from selenium.webdriver.common.keys import Keys
 
 LINKEDIN_URL = "https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=92000000&keywords=junior%20python%20developer&location=Worldwide"
 LINKEDIN_EMAIL = os.environ["LINKEDIN_EMAIL"]
@@ -12,7 +13,8 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get(LINKEDIN_URL)
 
 sleep(2)
-sign_in = driver.find_element_by_css_selector("body > div.base-serp-page > header > nav > div > a.nav__button-secondary")
+sign_in = driver.find_element_by_css_selector(
+    "body > div.base-serp-page > header > nav > div > a.nav__button-secondary")
 sign_in.click()
 
 sleep(2)
@@ -22,3 +24,17 @@ password = driver.find_element_by_name("session_password")
 password.send_keys(LINKEDIN_PASSWORD)
 submit = driver.find_element_by_css_selector("#organic-div > form > div.login__form_action_container > button")
 submit.click()
+
+sleep(2)
+save_button = driver.find_element_by_css_selector(".jobs-save-button")
+save_button.click()
+
+sleep(5)
+job_content = driver.find_element_by_css_selector("body > div.application-outlet > div.authentication-outlet > div.job-search-ext > div > div > section.jobs-search__right-rail")
+job_content.click()
+html = driver.find_element_by_tag_name("html")
+html.send_keys(Keys.END)
+
+sleep(2)
+follow_button = driver.find_element_by_css_selector(".jobs-company button")
+follow_button.click()
