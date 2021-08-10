eacc3b87eb7fded8f165f47c2d09b3b81748a36f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-09 21:56:01 -0400

    Import os and create constants

---
+++ internet-speed-twitter-bot/main.py
@@ -0,0 +1,7 @@
+import os
+
+PROMISED_DOWN = 100
+PROMISED_UP = 100
+CHROME_DRIVER_PATH = "/Users/nakoya/Development/chromedriver"
+TWITTER_EMAIL = os.environ["TWITTER_EMAIL"]
+TWITTER_PASSWORD = os.environ["TWITTER_PASSWORD"]
