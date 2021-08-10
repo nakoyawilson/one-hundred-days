3acb54195f6911bd510a3287c15bf09e91246126
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-10 08:13:30 -0400

    Add code for login method

--- instagram-follower/main.py
+++ instagram-follower/main.py
@@ -14,7 +14,16 @@ class InstaFollower:
         self.driver = webdriver.Chrome(executable_path=browser_driver_path)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        sleep(1)
+        username = self.driver.find_element_by_name("username")
+        username.send_keys(USERNAME)
+        sleep(1)
+        password = self.driver.find_element_by_name("password")
+        password.send_keys(PASSWORD)
+        sleep(1)
+        login_button = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
+        login_button.click()
 
     def find_followers(self):
         pass
@@ -27,3 +36,4 @@ insta_follower_bot = InstaFollower(CHROME_DRIVER_PATH)
 insta_follower_bot.login()
 insta_follower_bot.find_followers()
 insta_follower_bot.follow()
+
