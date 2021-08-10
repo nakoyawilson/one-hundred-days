0725eb124762be3373951c380942eace4e49bbf2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-09 23:11:22 -0400

    Add code for get_internet_speed() function

--- internet-speed-twitter-bot/main.py
+++ internet-speed-twitter-bot/main.py
@@ -1,5 +1,6 @@
 import os
 from selenium import webdriver
+from time import sleep
 
 PROMISED_DOWN = 100
 PROMISED_UP = 100
@@ -12,15 +13,24 @@ class InternetSpeedTwitterBot:
 
     def __init__(self, browser_driver_path):
         self.driver = webdriver.Chrome(executable_path=browser_driver_path)
-        self.up = 0
         self.down = 0
+        self.up = 0
 
     def get_internet_speed(self):
-        pass
+        self.driver.get("https://www.speedtest.net/")
+        start_test = self.driver.find_element_by_css_selector(".start-button")
+        sleep(2)
+        start_test.click()
+        sleep(60)
+        download_speed = self.driver.find_element_by_css_selector(".download-speed")
+        upload_speed = self.driver.find_element_by_css_selector(".upload-speed")
+        self.down = float(download_speed.text)
+        self.up = float(upload_speed.text)
+        sleep(2)
+        self.driver.quit()
 
     def tweet_at_provider(self):
         pass
-
 
 internet_speed_twitter_bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
 internet_speed_twitter_bot.get_internet_speed()
