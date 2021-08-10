7da0fc914657b9924355a4fbc79e5fdd1d6daa6c
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-10 07:58:39 -0400

    Create the InstaFollower class, initialise the object and call the three methods in order.

--- instagram-follower/main.py
+++ instagram-follower/main.py
@@ -1,6 +1,28 @@
 import os
+from selenium import webdriver
 
 CHROME_DRIVER_PATH = "/Users/nakoya/Development/chromedriver"
 SIMILAR_ACCOUNT = "python.hub"
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
+
+
+class InstaFollower:
+
+    def __init__(self, browser_driver_path):
+        self.driver = webdriver.Chrome(executable_path=browser_driver_path)
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+
+insta_follower_bot = InstaFollower()
+insta_follower_bot.login()
+insta_follower_bot.find_followers()
+insta_follower_bot.follow()
