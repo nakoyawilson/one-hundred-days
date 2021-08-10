abb45eb9bd816c7553ca10f20f8667838315b2b2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-09 22:06:26 -0400

    Create and initialise InternetSpeedTwitterBot

--- internet-speed-twitter-bot/main.py
+++ internet-speed-twitter-bot/main.py
@@ -1,7 +1,27 @@
 import os
+from selenium import webdriver
 
 PROMISED_DOWN = 100
 PROMISED_UP = 100
 CHROME_DRIVER_PATH = "/Users/nakoya/Development/chromedriver"
 TWITTER_EMAIL = os.environ["TWITTER_EMAIL"]
 TWITTER_PASSWORD = os.environ["TWITTER_PASSWORD"]
+
+
+class InternetSpeedTwitterBot:
+
+    def __init__(self, browser_driver_path):
+        self.driver = webdriver.Chrome(executable_path=browser_driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
+
+
+internet_speed_twitter_bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+internet_speed_twitter_bot.get_internet_speed()
+internet_speed_twitter_bot.tweet_at_provider()
