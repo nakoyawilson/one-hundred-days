433e802547850e700fd97384d3f3fd75d374cb68
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-14 23:20:04 -0400

    Create a new Flask application

---
+++ higher-lower/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start_game():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
+
+
+if __name__ == "__main__":
+    app.run()
