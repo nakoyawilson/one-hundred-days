bc6c0293b0205394db4a70e528b16027b0efeb4d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-16 15:05:25 -0400

    Add code to home() and blog_post()

--- blog-templating/main.py
+++ blog-templating/main.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c37f7c7d97c9c6067326"
+    response = requests.get(blog_url)
+    response.raise_for_status()
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/post/<int:blog_id>')
+def blog_post(blog_id):
+    post_index = blog_id - 1
+    blog_url = "https://api.npoint.io/c37f7c7d97c9c6067326"
+    response = requests.get(blog_url)
+    response.raise_for_status()
+    all_posts = response.json()
+    return render_template("post.html", posts=all_posts, index=post_index)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
