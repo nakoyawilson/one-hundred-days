8f75ea16bbb3b95a1db6b6e7f728509f9465886a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-17 09:31:36 -0400

    Render the about and contact pages

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -8,5 +8,15 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
