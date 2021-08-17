cd659bea0fa3a7ff5ba9e01319f40473697f6d2f
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-17 10:36:25 -0400

    Refactor code

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+blog_url = "https://api.npoint.io/e1249369ca6c46bc8d8f"
+response = requests.get(blog_url)
+response.raise_for_status()
+all_posts = response.json()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/about")
@@ -16,6 +22,12 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route('/post/<int:blog_id>')
+def blog_post(blog_id):
+    post_index = blog_id - 1
+    return render_template("post.html", posts=all_posts, index=post_index)
 
 
 if __name__ == "__main__":
