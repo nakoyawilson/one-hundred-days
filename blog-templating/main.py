5996237f8ebfdb786040d52a32e7018cf90b828d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-17 11:14:50 -0400

    Refactor code

--- blog-templating/main.py
+++ blog-templating/main.py
@@ -3,23 +3,20 @@ import requests
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/c37f7c7d97c9c6067326"
+response = requests.get(blog_url)
+response.raise_for_status()
+all_posts = response.json()
+
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/c37f7c7d97c9c6067326"
-    response = requests.get(blog_url)
-    response.raise_for_status()
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
 
 @app.route('/post/<int:blog_id>')
 def blog_post(blog_id):
     post_index = blog_id - 1
-    blog_url = "https://api.npoint.io/c37f7c7d97c9c6067326"
-    response = requests.get(blog_url)
-    response.raise_for_status()
-    all_posts = response.json()
     return render_template("post.html", posts=all_posts, index=post_index)
 
 
