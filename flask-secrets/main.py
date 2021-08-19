8e354e2f70ffb79a00940115817ee09959d2495b
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-19 11:11:04 -0400

    Create form and login

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,11 +1,35 @@
+import os
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = os.environ["SECRET_KEY"]
 
 
-@app.route("/")
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+@app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
