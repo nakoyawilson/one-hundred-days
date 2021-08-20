1e7c0ae04b3c504f0f4ac46b37054d0ff1c9389a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-20 08:56:46 -0400

    Click on Easy Apply and Next buttons

--- auto-job-application/main.py
+++ auto-job-application/main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from time import sleep
 import os
 
-LINKEDIN_URL = "https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=92000000&keywords=python&location=Worldwide"
+LINKEDIN_URL = "https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=92000000&keywords=junior%20python%20developer&location=Worldwide"
 LINKEDIN_EMAIL = os.environ["LINKEDIN_EMAIL"]
 LINKEDIN_PASSWORD = os.environ["LINKEDIN_PASSWORD"]
 
@@ -22,3 +22,17 @@ password = driver.find_element_by_name("session_password")
 password.send_keys(LINKEDIN_PASSWORD)
 submit = driver.find_element_by_css_selector("#organic-div > form > div.login__form_action_container > button")
 submit.click()
+
+# sleep(2)
+# job_listing = driver.find_element_by_css_selector(".job-card-container__link job-card-list__title")
+# job_listing.click()
+#
+sleep(2)
+easy_apply = driver.find_element_by_css_selector(".jobs-apply-button")
+easy_apply.click()
+
+sleep(2)
+next_button = driver.find_element_by_css_selector(".justify-flex-end .artdeco-button--primary")
+next_button.click()
+sleep(2)
+next_button.click()
