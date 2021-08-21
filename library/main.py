18edaf974572869750add26a3ab63fdd8436a246
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-21 00:21:11 -0400

    Make the form work

--- library/main.py
+++ library/main.py
@@ -7,12 +7,25 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_template('add.html')
+    if request.method == "POST":
+        title = request.form["title"]
+        author = request.form["author"]
+        rating = request.form["rating"]
+        book = {
+            "title": title,
+            "author": author,
+            "rating": rating
+        }
+        all_books.append(book)
+        print(all_books)
+        return render_template('add.html')
+    else:
+        return render_template('add.html')
 
 
 if __name__ == "__main__":
