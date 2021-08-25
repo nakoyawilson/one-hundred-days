a2b874fdc2afba186301a1262f897cbd8a951d9d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-25 12:34:37 -0400

    Add a new café to database

--- cafe-api/main.py
+++ cafe-api/main.py
@@ -37,6 +37,7 @@ def home():
     return render_template("index.html")
     
 
+# HTTP GET - Read Record
 @app.route("/random")
 def get_random_cafe():
     all_cafes = db.session.query(Cafe).all()
@@ -55,7 +56,6 @@ def get_all_cafes():
 @app.route("/search")
 def search_for_cafes():
     location = request.args.get("loc").title()
-    print(location)
     results = Cafe.query.filter_by(location=location).all()
     results_list = [create_cafe_dictionary(result) for result in results]
     error = {"Not Found": "Sorry, we don't have a cafe at that location."}
@@ -67,9 +67,28 @@ def search_for_cafes():
         return jsonify(cafes=results_list)
 
 
-# HTTP GET - Read Record
-
 # HTTP POST - Create Record
+@app.route("/add", methods=["GET", "POST"])
+def add_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("location"),
+        seats=request.form.get("seats"),
+        has_toilet=bool(request.form.get("has_toilet")),
+        has_wifi=bool(request.form.get("has_wifi")),
+        has_sockets=bool(request.form.get("has_sockets")),
+        can_take_calls=bool(request.form.get("can_take_calls")),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    success = {
+        "success": "Successfully added the new cafe."
+    }
+    return jsonify(response=success)
+
 
 # HTTP PUT/PATCH - Update Record
 
