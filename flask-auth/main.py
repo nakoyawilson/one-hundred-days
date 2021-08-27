f71cf96141c8b33a2ec7d5bd98786c56bc962d19
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-08-27 10:40:52 -0400

    Log in new user if registration successful or show flash message if not

--- flask-auth/main.py
+++ flask-auth/main.py
@@ -34,14 +34,20 @@ def register():
     if request.method == "POST":
         user_name = request.form["name"]
         password = generate_password_hash(request.form["password"], method='pbkdf2:sha256', salt_length=8)
-        new_user = User(
-            name=user_name,
-            email=request.form["email"],
-            password=password,
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template("secrets.html", name=user_name)
+        email = request.form["email"]
+        if User.query.filter_by(email=email).first():
+            flash("You've already signed up with that email, log in instead!")
+            return redirect("/register")
+        else:
+            new_user = User(
+                name=user_name,
+                email=email,
+                password=password,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect("/secrets")
     else:
         return render_template("register.html")
 
